e6ed304f82b0aa899ae36ed25c5e600cb0a98736
luis-mazo <luis.mzo@hotmail.com>
2026-02-23 00:25:19 -0300

    feat: initial structure with fonts and global styles

---
+++ src/mock/gifs.mock.ts
@@ -0,0 +1,52 @@
+export interface Gif {
+  id: string;
+  title: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+export const mockGifs: Gif[] = [
+  {
+    id: "1",
+    title: "Spongebob Squarepants",
+    url: "https://media3.giphy.com/media/v1.Y2lkPTgzNDY5YjhiZWdweWg2c21rcW1yaWJmMm00ZDhkM2txb2ZpNXQ3MGpzNmNobGRqdCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/kSIpGaEs4BTJaaGT69/giphy.gif",
+    width: 300,
+    height: 300,
+  },
+  {
+    id: "2",
+    title: "Funny Cat",
+    url: "https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
+    width: 400,
+    height: 250,
+  },
+  {
+    id: "3",
+    title: "Happy BDay!",
+    url: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcG40N2cyZm1iZGM2aWlhbHF1bnZmdzkxZ29idTY3ZzZkbnowNDg4OSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xatKdEypHxBjzabUPV/giphy.gif",
+    width: 280,
+    height: 350,
+  },
+  {
+    id: "4",
+    title: "Thumbs Up",
+    url: "https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif",
+    width: 300,
+    height: 200,
+  },
+  {
+    id: "5",
+    title: "Walter white",
+    url: "https://media2.giphy.com/media/v1.Y2lkPTgzNDY5YjhicTVrdTVhbGFqdnU2OW04YW9sdWJ6N2E0NDNzMHBqdnl1bjY1MnViZyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/y6Inkaz7omxAk/giphy.gif",
+    width: 300,
+    height: 400,
+  },
+  {
+    id: "6",
+    title: "Spongebob",
+    url: "https://media2.giphy.com/media/v1.Y2lkPTgzNDY5YjhiZWdweWg2c21rcW1yaWJmMm00ZDhkM2txb2ZpNXQ3MGpzNmNobGRqdCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/f1mbkSeiv2C9t2Pnbi/giphy.gif",
+    width: 350,
+    height: 280,
+  },
+];
